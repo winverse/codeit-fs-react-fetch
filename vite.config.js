8206035309365d0753ef0daa4db1dd388e4d092f
winverse <public.winverse@gmail.com>
2025-12-14 12:13:26 +0900

    feat: setup

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +11,9 @@ export default defineConfig({
       },
     }),
   ],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve('src'),
+    },
+  },
+});
